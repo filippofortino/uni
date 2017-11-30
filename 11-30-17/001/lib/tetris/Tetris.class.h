a91f55a663769279e7551708643922c913bff392
Filippo Fortino <filippofortino@gmail.com>
2017-11-30 10:10:09 +0100

    Remove: useless comment

--- 11-30-17/001/lib/tetris/Tetris.class.h
+++ 11-30-17/001/lib/tetris/Tetris.class.h
@@ -12,25 +12,3 @@ public:
     friend ostream& operator << (ostream& os, const Teris& t);
     bool operator == (const Tetris &t2)const;
 }
-
-/*
-struct elem {
-  char nome[31];
-  int pettorale;
-  elem* pun;
-};
-
-class Iscrizione
-{
-  elem* testa;
-
-public:
-  Iscrizione();
-  void visualizza()const;
-  bool aggiungi(const char* nome, int numero);
-
-  // SECONDA PARTE
-  bool cerca(const char* nome)const;
-  bool elimina(const char* nome);
-};
-*/
